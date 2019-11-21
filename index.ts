1a19732431f4f3d842b8e4af1e29e4abfcba40e1
Alexander Friedl <5493302+alexfriedl@users.noreply.github.com>
2019-11-21 12:56:49 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,23 +3,31 @@ import { Observable } from "rxjs";
 let counter = 1;
 const observable = Observable.create((observer: any) => {
   try {
+    observer.next('start')
     setInterval(() => {
       observer.next(counter++);
     }, 5);
   } catch (err) {
-    observer.error(err)
+    observer.error(err);
   }
 });
 
 const observer = observable.subscribe(
-  (val:any) => log(val),
-  (error:any) => log(error)
+  (val: any) => addItem(val),
+  (error: any) => log(error)
 );
 
 setTimeout(() => {
   observer.unsubscribe();
-}, 50)
+}, 50);
 
 function log(val) {
   console.log(val);
+}
+
+function addItem(val: any) {
+  let node = document.createElement('li');
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById('output').appendChild(node);
 }
