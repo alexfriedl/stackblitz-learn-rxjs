a48c9d0630c4e36940070001b72f96542ff96f42
alexfriedl <anecronika@gmx.de>
2019-11-21 14:08:43 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,34 +1,42 @@
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
-let counter = 1;
+let counter: number = 1;
+
 const observable = Observable.create((observer: any) => {
   try {
-    observer.next('start')
+    observer.next('start');
     setInterval(() => {
       observer.next(counter++);
-    }, 5);
+    }, 500);
   } catch (err) {
     observer.error(err);
   }
 });
 
 const observer = observable.subscribe(
-  (val: any) => addItem(val),
-  (error: any) => log(error)
+  (val: any) => addItem(val, 'First '),
+  (error: any) => log(error),
 );
+
+const observer2 = observable.subscribe(
+  (val: any) => addItem(val, 'Second '),
+  (error: any) => log(error),
+);
+
+observer.add(observer2);
 
 setTimeout(() => {
   observer.unsubscribe();
-}, 50);
+}, 2500);
 
 function log(val) {
   console.log(val);
 }
 
-function addItem(val: any) {
+function addItem(val:any, name) {
   let node = document.createElement('li');
-  var textnode = document.createTextNode(val);
+  var textnode = document.createTextNode(name + val);
   node.appendChild(textnode);
   document.getElementById('output').appendChild(node);
+  console.log(val);
 }
-
