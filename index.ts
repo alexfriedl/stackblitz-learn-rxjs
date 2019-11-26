5571cf45b83a4a8149ffe359d8c29db9b390e9cc
alexfriedl <anecronika@gmx.de>
2019-11-26 17:28:54 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -4,8 +4,15 @@ import 'rxjs/add/operator/share';
 let counter: number = 1;
 let name: string = "I am good";
 
+// Create an observable
+// A stream of values you can observe
+// 
+// 1. const observable = Observable.create() - create method
+// 2. const observable = new Observable() - instantiation
+// 3. const observable = fromEvent(document, mouseover) - calling  operators 
 const observable = Observable.create((observer: any) => {
   try {
+    // next(value) adds value to the stream
     observer.next("Hii");
     observer.next("How are you");
     setInterval(() => {
@@ -16,6 +23,10 @@ const observable = Observable.create((observer: any) => {
   }
 }).share();
 
+// Create an observer
+// A subscription of an observable that displays its data
+// const observer = observable.subscribe()
+// Pass value, error and completed as arguments
 setTimeout(() => {
   const observer = observable.subscribe(
     (val: any) => addItem(val),
@@ -23,12 +34,14 @@ setTimeout(() => {
   );
 }, 1500);
 
+// Same as above (created a second observer)
 setTimeout(() => {
   const observer2 = observable.subscribe((val: any) =>
     addItem("2: " + val)
   );
 }, 1000)
 
+// Just UI
 function addItem(val: any) {
   let node = document.createElement("li");
   var textnode = document.createTextNode(val);
